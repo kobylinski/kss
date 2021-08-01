29a82ea0d7bdb23aa2f8497bfa40f705fe1fe048
Marek Kobylinski <marek@kobylinski.co>
2021-08-01 06:04:43 +0200

    feat: see commit before

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
+Builder for [kss-node](https://github.com/kss-node/kss-node) generator.
+
 # Install
+
 ```
-$ npm install niepsuj-kss-template
+npm install niepsuj-kss-template
+yarn add niepsuj-kss-template
 ```
 
 # Use
@@ -8,9 +12,13 @@ $ npm install niepsuj-kss-template
 ```
 $ kss \
     --source "source folder" \
-    --destination "destination folder" \ 
+    --destination "destination folder" \
     --builder "path to template" \
-    --custom "custom html - footer logo" \
-    --js "list/of,js/files,comma/separated" \
-    --css "list/of,css/files,comma/separated"
+    --custom "path/to/footer.hbs" \
+    --js "path/to/js/file.js" \
+    --js "path/to/another/js/file.js" \
+    --css "path/to/css/file.css" \
+    --css "path/to/another/css/file.css"
 ```
+
+- `--custom` - It can be string or HTML placed in orange footer below the navigation. It can be also path to footer.hbs file. In that case this file will be compiled with package.json file as an input.
--- builder/builder.js
+++ builder/builder.js
@@ -1,99 +1,122 @@
-'use strict';
+"use strict";
 
-const KssBuilderBase = require('kss/builder/base/handlebars');
-const hljs = require('highlight.js');
-
-
+const KssBuilderBase = require("kss/builder/base/handlebars");
+const hljs = require("highlight.js");
+const fs = require("fs");
+const path = require("path");
 class Builder extends KssBuilderBase {
-
-	constructor() {
+  constructor() {
     super();
 
-    // Store the version of the builder API that the builder instance is
-    // expecting; we will verify this in loadBuilder().
-    this.API = '3.0';
-
-    // this.addOptionDefinitions({
-    //   'example-option': {
-    //     string: true,
-    //     description: 'This is a custom command-line option used by this Builder.'
-    //   }
-    // });
+    this.API = "3.0";
   }
 
   prepare(styleGuide) {
-    return super.prepare(styleGuide).then(styleGuide => {
+    return super.prepare(styleGuide).then((styleGuide) => {
+      var _hbs = this.Handlebars;
 
-    	var _hbs = this.Handlebars;
+      this.Handlebars.registerHelper({
+        eq: function (v1, v2) {
+          return v1 === v2;
+        },
+        ne: function (v1, v2) {
+          return v1 !== v2;
+        },
+        lt: function (v1, v2) {
+          return v1 < v2;
+        },
+        gt: function (v1, v2) {
+          return v1 > v2;
+        },
+        lte: function (v1, v2) {
+          return v1 <= v2;
+        },
+        gte: function (v1, v2) {
+          return v1 >= v2;
+        },
+        and: function (v1, v2) {
+          return v1 && v2;
+        },
+        or: function (v1, v2) {
+          return v1 || v2;
+        },
+      });
 
-    	this.Handlebars.registerHelper({
-			eq: function (v1, v2) {
-				return v1 === v2;
-			},
-			ne: function (v1, v2) {
-				return v1 !== v2;
-			},
-			lt: function (v1, v2) {
-				return v1 < v2;
-			},
-			gt: function (v1, v2) {
-				return v1 > v2;
-			},
-			lte: function (v1, v2) {
-				return v1 <= v2;
-			},
-			gte: function (v1, v2) {
-				return v1 >= v2;
-			},
-			and: function (v1, v2) {
-				return v1 && v2;
-			},
-			or: function (v1, v2) {
-				return v1 || v2;
-			}
-		});
+      this.Handlebars.registerHelper("SYNTAX", function (options) {
+        var output;
+        if (this.markup.endsWith(".hbs")) {
+          var template = _hbs.compile(
+            "{{> " + this.markup.replace(".hbs", "") + " }}"
+          );
+          output = template();
+        } else {
+          output = this.markup;
+        }
+        return new _hbs.SafeString(
+          hljs.highlight(output, { language: "html" }).value
+        );
+      });
 
-		this.Handlebars.registerHelper('SYNTAX', function(options){
-			var output;
-			if (this.markup.endsWith('.hbs')) {
-				template = _hbs.compile('{{> ' + this.markup.replace('.hbs', '') + ' }}');
-				output = template();
-		    } else {
-		      	output = this.markup;
-		    }
-			return new _hbs.SafeString(hljs.highlight(output, {language: 'html'}).value);
-		});
+      this.Handlebars.registerHelper("LINK", function (content) {
+        return content.replace(
+          /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim,
+          '<a href="mailto:$1">$1</a>'
+        );
+      });
 
-		this.Handlebars.registerHelper('CSS', function(string){
-			if(!string.length){
-				return '';
-			}
+      this.Handlebars.registerHelper("CUSTOM", function (place, input) {
+        var tmpl = input.find(function (el) {
+          if (el.endsWith(".hbs")) {
+            var p = path.resolve(el);
+            if (fs.existsSync(p)) {
+              return place === path.basename(p, ".hbs");
+            }
+          }
+          return false;
+        });
 
-			var data = string[0].split(',');
-			var result = [];
-			for(var i = 0; i < data.length; i++){
-				if(data[i].split('.').pop() == 'css'){
-					result.push('<link rel="stylesheet" href="'+data[i]+'">');
-				}
-			}
+        if (tmpl) {
+          var p = path.resolve(tmpl);
+          var template = _hbs.compile(fs.readFileSync(p).toString("utf8"), {
+            noEscape: true,
+          });
+          return template(require(path.resolve("package.json")));
+        }
+        return input;
+      });
 
-			return result.join("\n\t");
-		});
+      this.Handlebars.registerHelper("CSS", function (string) {
+        if (!string.length) {
+          return "";
+        }
 
-		this.Handlebars.registerHelper('JS', function(string){
-			if(!string.length){
-				return '';
-			}
+        console.log(string);
 
-			var data = string[0].split(',');
-			var result = [];
-			for(var i = 0; i < data.length; i++){
-				if(data[i].split('.').pop() == 'js'){
-					result.push('<script src="'+data[i]+'"></script>');
-				}
-			}
-			return result.join("\n\t");
-		});
+        var data = string[0].split(",");
+        var result = [];
+        for (var i = 0; i < data.length; i++) {
+          if (data[i].split(".").pop() == "css") {
+            result.push('<link rel="stylesheet" href="' + data[i] + '">');
+          }
+        }
+
+        return result.join("\n\t");
+      });
+
+      this.Handlebars.registerHelper("JS", function (string) {
+        if (!string.length) {
+          return "";
+        }
+
+        var data = string[0].split(",");
+        var result = [];
+        for (var i = 0; i < data.length; i++) {
+          if (data[i].split(".").pop() == "js") {
+            result.push('<script src="' + data[i] + '"></script>');
+          }
+        }
+        return result.join("\n\t");
+      });
 
       return Promise.resolve(styleGuide);
     });
